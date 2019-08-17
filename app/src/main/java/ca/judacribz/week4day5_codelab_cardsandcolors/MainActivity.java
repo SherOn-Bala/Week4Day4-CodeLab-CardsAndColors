843154b9bb6ef35f100d41c73e4f46062933b5c7
SherOn-Bala <jude.balasingam@mobileappscompany.us>
2019-08-17 13:28:11 -0400

    init

---
+++ app/src/main/java/ca/judacribz/week4day5_codelab_cardsandcolors/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.judacribz.week4day5_codelab_cardsandcolors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
