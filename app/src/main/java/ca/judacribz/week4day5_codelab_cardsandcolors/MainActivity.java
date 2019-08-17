312f4fbe215a441e8002805eb112ecba637e02ea
SherOn-Bala <jude.balasingam@mobileappscompany.us>
2019-08-17 17:33:42 -0400

    finished codelab

--- app/src/main/java/ca/judacribz/week4day5_codelab_cardsandcolors/MainActivity.java
+++ app/src/main/java/ca/judacribz/week4day5_codelab_cardsandcolors/MainActivity.java
@@ -1,17 +1,24 @@
 package ca.judacribz.week4day5_codelab_cardsandcolors;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Member variables
-    private RecyclerView mRecyclerView;
+    private static final String EXTRA_SPORTS = "ca.judacribz.week4day5_codelab_cardsandcolors.EXTRA_SPORTS";
     private ArrayList<Sport> mSportsData;
     private SportsAdapter mAdapter;
 
@@ -20,41 +27,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initialize the RecyclerView
-        mRecyclerView = findViewById(R.id.recyclerView);
-
-        //Set the Layout Manager
+        RecyclerView mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //Initialize the ArrayLIst that will contain the data
         mSportsData = new ArrayList<>();
-
-        //Initialize the adapter and set it ot the RecyclerView
         mAdapter = new SportsAdapter(this, mSportsData);
         mRecyclerView.setAdapter(mAdapter);
 
-        //Get the data
         initializeData();
+
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT |
+                        ItemTouchHelper.RIGHT |
+                        ItemTouchHelper.DOWN |
+                        ItemTouchHelper.UP
+        ) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView,
+                                  @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder target) {
+
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                Collections.swap(mSportsData, from, to);
+                mAdapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                mSportsData.remove(viewHolder.getAdapterPosition());
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+
+        helper.attachToRecyclerView(mRecyclerView);
+
     }
 
-    /**
-     * Method for initializing the sports data from resources.
-     */
     private void initializeData() {
-        //Get the resources from the XML file
-        String[] sportsList = getResources().getStringArray(R.array.sports_titles);
-        String[] sportsInfo = getResources().getStringArray(R.array.sports_info);
-
-        //Clear the existing data (to avoid duplication)
+        String[]
+                sportsList = getResources().getStringArray(R.array.sports_titles),
+                sportsInfo = getResources().getStringArray(R.array.sports_info);
+        TypedArray sportsImageResources =
+                getResources().obtainTypedArray(R.array.sports_images);
         mSportsData.clear();
-
-        //Create the ArrayList of Sports objects with the titles and information about each sport
-        for(int i=0;i<sportsList.length;i++){
-            mSportsData.add(new Sport(sportsList[i],sportsInfo[i]));
+        for (int i = 0; i < sportsList.length; i++) {
+            mSportsData.add(new Sport(
+                    sportsList[i],
+                    sportsInfo[i],
+                    sportsImageResources.getResourceId(i, 0)
+            ));
         }
 
-        //Notify the adapter of the change
+        sportsImageResources.recycle();
         mAdapter.notifyDataSetChanged();
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        outState.putParcelableArrayList(EXTRA_SPORTS, mSportsData);
+    }
+
+    @Override
+    public void onRestoreInstanceState(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        if (savedInstanceState != null) {
+            mSportsData = savedInstanceState.getParcelableArrayList(EXTRA_SPORTS);
+            mAdapter.notifyDataSetChanged();
+            Toast.makeText(this, "restore", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void resetSports(View view) {
+        initializeData();
+    }
 }
--- app/src/main/java/ca/judacribz/week4day5_codelab_cardsandcolors/Sport.java
+++ app/src/main/java/ca/judacribz/week4day5_codelab_cardsandcolors/Sport.java
@@ -1,40 +1,58 @@
 package ca.judacribz.week4day5_codelab_cardsandcolors;
 
-/**
- * Data model for each row of the RecyclerView.
- */
-class Sport {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    //Member variables representing the title and information about the sport
+class Sport implements Parcelable {
     private String title;
     private String info;
+    private final int imageResource;
 
-    /**
-     * Constructor for the Sport data model
-     *
-     * @param title The name if the sport.
-     * @param info  Information about the sport.
-     */
-    Sport(String title, String info) {
+    public Sport(String title, String info, int imageResource) {
         this.title = title;
         this.info = info;
+        this.imageResource = imageResource;
     }
 
-    /**
-     * Gets the title of the sport
-     *
-     * @return The title of the sport.
-     */
+    protected Sport(Parcel in) {
+        title = in.readString();
+        info = in.readString();
+        imageResource = in.readInt();
+    }
+
+    public static final Creator<Sport> CREATOR = new Creator<Sport>() {
+        @Override
+        public Sport createFromParcel(Parcel in) {
+            return new Sport(in);
+        }
+
+        @Override
+        public Sport[] newArray(int size) {
+            return new Sport[size];
+        }
+    };
+
     String getTitle() {
         return title;
     }
 
-    /**
-     * Gets the info about the sport
-     *
-     * @return The info about the sport.
-     */
     String getInfo() {
         return info;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(info);
+        parcel.writeInt(imageResource);
     }
 }
